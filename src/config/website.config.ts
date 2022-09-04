5121d043a7a115882ae02c43d3698fbf95f38429
LingLuoYi <32977408+LingLuoYi@users.noreply.github.com>
2022-09-04 21:57:16 +0800

    login

---
+++ src/config/website.config.ts
@@ -0,0 +1,9 @@
+import logoImage from '@/assets/images/logo.png';
+import loginImage from '@/assets/images/account-logo.png';
+
+export const websiteConfig = Object.freeze({
+  title: '粤文交',
+  logo: logoImage,
+  loginImage: loginImage,
+  loginDesc: 'LingLouYi',
+});
